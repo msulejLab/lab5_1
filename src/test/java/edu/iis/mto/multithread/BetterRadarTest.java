315269086a641674f5006480be834267c7771f4a
Maciej Sulej <187809@edu.p.lodz.pl>
2016-05-19 17:38:04 +0200

    Wielokrotne uruchamianie testow

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.util.concurrent.Executor;
@@ -9,7 +10,14 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 public class BetterRadarTest {
+
+    private static int testTimeRun = 0;
+
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @Repeat(10)
     public void testLauncherInTheSameThread() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Executor executor = new Executor() {
@@ -18,10 +26,13 @@ public class BetterRadarTest {
                 runnable.run();
             }
         };
+
         BetterRadar radar = new BetterRadar(batteryMock, executor);
 
         radar.notice(new Scud());
 
         verify(batteryMock, times(10)).launchPatriot();
+
+        System.out.println("Test run: "  + testTimeRun++);
     }
 }
